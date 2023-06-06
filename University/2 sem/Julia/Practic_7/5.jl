861c339da8ba1202114b3d402356571033cb780b
Vlad1000nn <vladislav.nevsky.05@mail.ru>
2023-06-06 10:25:01 +0300

    .

--- University/2 sem/Julia/Practic_7/5.jl
+++ University/2 sem/Julia/Practic_7/5.jl
@@ -1,12 +1,41 @@
 # Проверяем граф на двудольность
 using LightGraphs
 
-# Создаем граф
+function is_bipartite(g)
+    # Создаем массив цветов для каждой вершины графа
+    colors = fill(0, nv(g))
+    
+    # Функция для проверки двудольности графа
+    function dfs(v, color)
+        colors[v] = color
+        for w in neighbors(g, v)
+            if colors[w] == color
+                return false
+            elseif colors[w] == 0 && !dfs(w, -color)
+                return false
+            end
+        end
+        return true
+    end
+    
+    # Проверяем каждую вершину графа
+    for v in vertices(g)
+        if colors[v] == 0 && !dfs(v, 1)
+            return false
+        end
+    end
+    
+    return true
+end
+
 g = SimpleGraph(4)
 add_edge!(g, 1, 2)
 add_edge!(g, 2, 3)
 add_edge!(g, 3, 4)
 add_edge!(g, 4, 1)
 
-# Проверяем граф на двудольность
-is_bipartite(g)
+if is_bipartite(g)
+    println("Граф является двудольным")
+else
+    println("Граф не является двудольным")
+end

296fc6d9fe5f7667a461597f92f4b2a8b58f2e98
Vlad1000nn <vladislav.nevsky.05@mail.ru>
2024-03-17 19:54:54 +0300

    .

---
+++ Acmp/401.cpp
@@ -0,0 +1,238 @@
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("avx,avx2,fma")
+//#pragma GCC optimize("trapv")
+//#include <bits/stdc++.h>
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <numeric>
+#include <algorithm>
+
+#include <chrono>
+#include <ctime>
+#include <random>
+
+#include <list>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <deque>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <map>
+#include <queue>
+#include <cassert>
+#include <algorithm>
+#include <functional>
+
+#include <fstream>
+#include <sstream>
+
+
+#define isz(x) (int)x.size()
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+
+template<typename T>
+using PriorityQueue = priority_queue<T, vector<T>, greater<T>>;
+
+template<typename T>
+using MAP = map<T, T, greater<T>>;
+
+template<typename T>
+using SET = set<T, T, greater<T>>;
+
+template<typename T>
+using MULTISET = multiset<T, T, greater<T>>;
+
+
+
+using ll = long long;
+using vll = vector<long long>;
+using vvll = vector<vll>;
+
+using ull = unsigned long long;
+
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vvvi = vector<vvi>;
+
+namespace Random
+{
+    std::mt19937 mt{ std::random_device{}() };
+
+    int get(int min, int max)
+    {
+        std::uniform_int_distribution<> die{ min, max };
+        return die(mt);
+    }
+}
+
+
+class Timer
+{
+private:
+    using Clock = std::chrono::steady_clock;
+    using Second = std::chrono::duration<double, std::ratio<1> >;
+    std::chrono::time_point<Clock> m_beg{ Clock::now() };
+public:
+
+    void reset()
+    {
+        m_beg = Clock::now();
+    }
+
+    double elapsed() const
+    {
+        return std::chrono::duration_cast<Second>(Clock::now() - m_beg).count();
+    }
+};
+
+
+template<typename T>
+std::istream& operator>>(std::istream& in, vector<T>& vec)
+{
+    for (auto& it : vec)
+        in >> it;
+    return in;
+}
+
+// Overload vector output
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec)
+{
+    for (auto& it : vec)
+        out << it << ' ';
+
+    return out;
+}
+
+
+ll binpow(ll a, ll n)
+{
+    if (!n) return 1;
+
+    ll res{ 1 };
+    while (n)
+    {
+        if (n & 1)
+            res *= a;
+        a *= a;
+        n >>= 1;
+    }
+    return res;
+}
+
+ll binpow(ll a, ll n, ll mod)
+{
+    if (!n) return 1;
+
+    ll res = 1 % mod;
+    while (n)
+    {
+        if (n & 1)
+        {
+            res *= a;
+            res %= mod;
+        }
+        a *= a;
+        a %= mod;
+        n >>= 1;
+
+    }
+
+    return res % mod;
+}
+
+
+// РџРµСЂРµР±РѕСЂ РЅР°РїСЂР°РІР»РµРЅРёР№
+const int N[4][2] = {
+          {-1,0}, {0, 1}, {1,0}, {0,-1}
+};
+
+const int INF = int(1e9);
+const ll INFL = ll(1e18);
+const ull INFUL = ull(1e19);
+
+const ll MOD = 1000000000 + 7;
+
+const int UNDEF = -1;
+////////////////////////////////////////////////////////////////////////////////////
+
+using vpii = vector<pair<int, int >>;
+using vpll = vector<pair<ll, ll>>;
+
+
+
+//#define int ll
+
+
+__int128_t read(){
+    __int128_t x=0;bool f=0;char c=getchar();
+    while (c<'0'||c>'9'){if (c=='-')f=1;c=getchar();}
+    while (c>='0'&&c<='9'){x=(x<<1)+(x<<3)+(c^48);c=getchar();}
+    return f?-x:x;
+}
+
+inline void write(__int128_t x)
+{
+     if(x<0) putchar('-'),x=-x;
+     if(x>9) write(x/10);
+     putchar(x%10+'0');
+}
+
+
+void solve()
+{
+    int n, R, B; cin >> n >> R >> B;
+
+    vector<vector<vector<__int128_t>>> dp(n + 1, vector<vector<__int128_t>>(R + 1, vector<__int128_t>(B + 1)));
+    dp[0][0][0] = 1;
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int r = 0; r <= R; ++r)
+        {
+            for (int b = 0; b <= B; ++b)
+            {
+                for (int dr = 0; r + dr <= R; ++dr)
+                {
+                    for (int db = 0; b + db <= B; ++db)
+                    {
+                        dp[i + 1][r + dr][b + db] += dp[i][r][b];
+                    }
+                }
+            }
+        }
+    }
+
+    __int128_t answ{};
+    for (int i = 0; i <= R; ++i)
+        for (int j = 0; j <= B; ++j)
+            answ += dp[n][i][j];
+
+    write(answ);
+
+}
+
+
+
+int32_t main()
+{
+    cin.tie(0);
+    std::ios_base::sync_with_stdio(false);
+
+    int32_t t{ 1 };
+    //cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}

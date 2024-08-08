1dfa4aafb98310b6ce709ee7b427ac0e7c0c6a13
Vlad1000nn <vladislav.nevsky.05@mail.ru>
2024-08-09 01:24:23 +0300

    .

--- MyRealization/Structures/DSU/Codeforces_EDU.cpp
+++ MyRealization/Structures/DSU/Codeforces_EDU.cpp
@@ -307,3 +307,417 @@ void solve()
         cout << (it ? "YES\n" : "NO\n");
 }
 
+
+// Monkeys
+
+
+class DSU
+{
+private:
+ 
+    vi sz, parent, time, left, right;
+ 
+ 
+    int getParent(int u)
+    {
+        return (u == parent[u] ? u : getParent(parent[u]));
+    }
+ 
+public:
+ 
+    DSU(const int n)
+        : sz(n+1,1)
+        , parent(n+1)
+        , time(n+1, INF)
+    {
+        std::iota(all(parent), 0);
+    }
+ 
+    void merge(int x, int y, int currTime = INF)
+    {
+        if (x == y || x == -1 || y == -1)
+            return;
+ 
+        x = getParent(x);
+        y = getParent(y);
+ 
+        if (x == y)
+            return;
+ 
+        if (sz[x] < sz[y])
+            std::swap(x, y);
+ 
+        if (x == 1)
+        {
+            setmin(time[y], currTime);
+        }
+        else if (y == 1)
+        {
+            setmin(time[x], currTime);
+            std::swap(x, y);
+        }
+        else {
+            setmin(time[y], currTime);
+        }
+ 
+        parent[y] = x;
+        sz[x] += sz[y];
+ 
+    }
+ 
+    void update(const int n)
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            int curr = i;
+            while (curr != parent[curr])
+            {
+                setmin(time[i], time[curr]);
+                curr = parent[curr];
+            }
+        }
+    }
+ 
+    const vi& get()   const
+    {
+        return time;
+    }
+ 
+};
+ 
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    DSU dsu(n);
+ 
+    vi left(n + 1), right(n + 1);
+    for (int i = 1; i <= n; ++i)
+        cin >> left[i] >> right[i];
+ 
+    vpii queries(m);
+    for (auto& [first, second] : queries)
+        cin >> first >> second;
+    
+    vi buffl = left, buffr = right;
+ 
+    for (auto& [monkey, hand] : queries)
+        hand == 1 ? left[monkey] = -1 : right[monkey] = -1;
+ 
+    for (int i = 1; i <= n; ++i)
+    {
+        if (left[i] != -1)
+            dsu.merge(i, left[i]);
+        if (right[i] != -1)
+            dsu.merge(i, right[i]);
+    }
+ 
+    reverse(all(queries));
+ 
+    int currTime = m - 1;
+    for (auto& [monkey, hand] : queries)
+    {
+        hand == 1 ? left[monkey] = buffl[monkey] : right[monkey] = buffr[monkey];
+        dsu.merge(monkey, hand == 1 ? left[monkey] : right[monkey], currTime);
+        --currTime;
+    }
+ 
+    dsu.update(n);
+ 
+    vi answ = dsu.get();
+    for (int i = 1; i <= n; ++i)
+        std::cout << (answ[i] == INF ? -1 : answ[i]) << '\n';
+ 
+}
+
+/* Step 2 */
+
+// People are leaving
+
+class DSU
+{
+private:
+ 
+    vi sz, parent;
+ 
+ 
+    int getParent(int x)
+    {
+        return (x == parent[x] ? x : parent[x] = getParent(parent[x]));
+    }
+ 
+public:
+ 
+    DSU(const int n)
+        : sz(n+1,1)
+        , parent(n+2)
+    {
+        std::iota(all(parent), 0);
+    }
+ 
+    void merge(int x)
+    {
+        int y = x + 1;
+        x = getParent(x);
+        y = getParent(y);
+ 
+        parent[x] = y;
+    }
+ 
+    int get(int x)
+    {
+        return (getParent(x) == isz(parent) - 1 ? -1 : getParent(x));
+    }
+ 
+ 
+};
+ 
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    DSU dsu(n);
+ 
+    const char MINUS = '-', GET = '?';
+    while (m--)
+    {
+        char type; cin >> type;
+        if (MINUS == type)
+        {
+            int x; cin >> x;
+            dsu.merge(x);
+        }
+        else if (GET == type)
+        {
+            int x; cin >> x;
+            std::cout << dsu.get(x) << '\n';
+        }
+        else throw 1;
+    }
+}
+
+
+// Parking
+
+class DSU
+{
+private:
+ 
+    vi sz, parent;
+ 
+ 
+    int getParent(int x)
+    {
+        return (x == parent[x] ? x : parent[x] = getParent(parent[x]));
+    }
+ 
+public:
+ 
+    DSU(const int n)
+        : sz(n+1,1)
+        , parent(n+1)
+    {
+        std::iota(all(parent), 0);
+    }
+ 
+    void merge(int x, int y)
+    {
+        x = getParent(x);
+        y = getParent(y);
+        if (sz[x] < sz[y])
+            std::swap(x, y);
+ 
+        parent[y] = x;
+        sz[x] += sz[y];
+    }
+ 
+    int add(int x)
+    {
+        x = getParent(x);
+        int y = (x + 1 == isz(parent) ? getParent(1) : getParent(x + 1));
+        parent[x] = y;
+        sz[y] += sz[x];
+ 
+        return x;
+    }
+ 
+};
+ 
+ 
+void solve()
+{
+    int n; cin >> n;
+    DSU dsu(n);
+ 
+    while (n--)
+    {
+        int place; cin >> place;
+        cout << dsu.add(place) << ' ';
+    }
+ 
+}
+
+// Spannig Tree
+
+
+class DSU
+{
+private:
+ 
+    vi sz, parent;
+ 
+ 
+    int getParent(int x)
+    {
+        return (x == parent[x] ? x : parent[x] = getParent(parent[x]));
+    }
+ 
+public:
+ 
+    DSU(const int n)
+        : sz(n+1,1)
+        , parent(n+2)
+    {
+        std::iota(all(parent), 0);
+    }
+ 
+    void merge(int x, int y)
+    {
+        x = getParent(x);
+        y = getParent(y);
+        if (sz[x] < sz[y])
+            std::swap(x, y);
+ 
+        parent[y] = x;
+        sz[x] += sz[y];
+    }
+ 
+    bool get(int x, int y)
+    {
+        return getParent(x) == getParent(y);
+    }
+ 
+ 
+};
+ 
+struct Edge
+{
+    int w, u, v;
+ 
+    bool operator<(const Edge& edge)
+    {
+        return this->w < edge.w;
+    }
+ 
+    friend std::istream& operator>>(std::istream& in, Edge& edge)
+    {
+        in >> edge.u >> edge.v >> edge.w;
+        return in;
+    }
+};
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    DSU dsu(n);
+    ll answ = 0;
+ 
+    vector<Edge> edges(m);
+    cin >> edges;
+    
+    sort(all(edges));
+ 
+    for (auto& it : edges)
+    {
+        int u = it.u;
+        int v = it.v;
+        int w = it.w;
+ 
+        if (!dsu.get(u, v)) {
+            dsu.merge(u, v);
+            answ += w;
+        }
+    }
+ 
+ 
+    std::cout << answ;
+ 
+}
+
+
+// First Non-Bipartite Edge
+
+
+class DSU
+{
+private:
+ 
+    vi sz, parent, len;
+ 
+ 
+    std::pair<int,int> getParent(int x)
+    {
+        if (x == parent[x])
+            return { x,0 };
+        std::pair<int, int> val = getParent(parent[x]);
+        parent[x] = val.first;
+        len[x] = (len[x] + val.second) % 2;
+        return { parent[x], len[x] };
+    }
+ 
+public:
+ 
+    DSU(const int n)
+        : sz(n+1,1)
+        , parent(n+1)
+        , len(n+1)
+    {
+        std::iota(all(parent), 0);
+    }
+ 
+    bool merge(int x, int y)
+    {
+        std::pair<int, int> valX = getParent(x);
+        std::pair<int, int> valY = getParent(y);
+ 
+        if (valX.first == valY.first)
+        {
+            return valX.second != valY.second;
+        }
+ 
+        if (sz[valX.first] < sz[valY.first])
+            std::swap(valX, valY);
+ 
+ 
+        parent[valY.first] = valX.first;
+        len[valY.first] = (1 + valX.second + valY.second) % 2;
+        sz[valX.first] += sz[valY.first];
+ 
+        return  1;
+    }
+ 
+   
+ 
+};
+ 
+ 
+void solve()
+{
+    int n, m; cin >> n >> m;
+    DSU dsu(n);
+    int answ{-1};
+ 
+    for (int i = 1; i <= m; ++i)
+    {
+        int u, v; cin >> u >> v;
+        if (!dsu.merge(u, v) && answ == -1)
+        {
+            answ = i;
+        }
+    }
+ 
+    std::cout << answ;
+ 
+ 
+ 
+}

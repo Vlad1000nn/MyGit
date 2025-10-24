5c062e0b81acaf81d77ba50ef5c389260a1b0007
Vlad1000nn <117930947+Vlad1000nn@users.noreply.github.com>
2025-10-25 00:34:49 +0300

    First try G. Ё-Тауэр

---
+++ competitive/MRC-Qual-2025/G.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <cstdio>
+#include "stdio.h"
+#include <cmath>
+#include <iomanip>
+#include <numeric>
+#include <algorithm>
+
+#include <chrono>
+#include <ctime>
+#include <random>
+
+#include <list>
+#include <array>
+#include <string>
+#include <vector>
+#include <bitset>
+#include <set>
+#include <queue>
+#include <deque>
+#include <map>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <map>
+#include <queue>
+#include <cassert>
+#include <functional>
+
+#include <fstream>
+#include <sstream>
+
+//#pragma GCC optimize("Ofast,unroll-loops")
+//#include <bits/stdc++.h>
+//#pragma GCC target("avx,avx2,fma")
+
+#define isz(x) (int)(x).size()
+#define all(x) (x).begin(),(x).end()
+
+using namespace std;
+
+using ll = long long;
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+bool str_comp(const string& s1, const string& s2)
+{
+    const int n = isz(s1);
+    const int m = isz(s2);
+    if (n != m)
+        return n < m;
+
+    return s1 < s2;
+}
+
+void solve()
+{
+    string curr, buff;
+    int k = 0;
+    string s; cin >> s;
+    const int n = isz(s);
+    if (n == 1) {
+        cout << s << '\n'; return;
+    }
+    for (; k * k <= n; ++k)
+        ;
+    k = min(n, k);
+
+    vvi pref_dp1(n + 1, vi(k + 1));
+    vvi pref_dp2(n + 1, vi(k + 1));
+
+    vvi dp1(n, vi(k + 1));
+    vvi dp2(n, vi(k + 1));
+
+    for (int i = 0; i <= k; ++i)
+        dp1[0][i] = dp2[0][i] = 1;
+    pref_dp1[0][0] = pref_dp2[0][k] = 1;
+    for (int i = 1; i <= k; ++i)
+        pref_dp1[0][i] = pref_dp1[0][i - 1] + 1;
+    for (int i = 1; i <= k; ++i)
+        pref_dp2[0][i] = pref_dp2[0][i - 1] + 1;
+
+    for (int i = 1; i <= n; ++i) {
+        curr = "";
+        for (int j = 1; j <= k; ++j) {
+            if (i - j < 0) break;
+            curr += s[i - j];
+
+            if (pref_dp1[i - j][j - 1]) {
+                dp1[i - 1][j] = 1;
+            }
+            else if (i - 2 * j >= 0) {
+                int k = j;
+                buff = curr;
+                reverse(all(buff));
+                if (dp1[i - j - 1][k] == 1 && str_comp(s.substr(i - j - k, k), buff))
+                    dp1[i - 1][j] = 1;
+            }
+            pref_dp1[i][j] = pref_dp1[i][j - 1] + dp1[i - 1][j];
+        }
+    }
+
+    string s_rev = s;
+    reverse(all(s_rev));
+
+    for (int i = 1; i <= n; ++i) {
+        curr = "";
+        for (int j = 1; j <= k; ++j) {
+            if (i - j < 0) break;
+            curr += s_rev[i - j];
+
+            if (pref_dp2[i - j][j - 1]) {
+                dp2[i - 1][j] = 1;
+            }
+            else if (i - 2 * j >= 0) {
+                int k = j;
+                buff = curr;
+                reverse(all(buff));
+                if (dp1[i - j - 1][k] == 1 && str_comp(s_rev.substr(i - j - k, k), buff))
+                    dp2[i - 1][j] = 1;
+            }
+            pref_dp2[i][j] = pref_dp2[i][j - 1] + dp2[i - 1][j];
+        }
+    }
+
+
+    string ans = std::string(n + 1, '9');
+    for (int i = 0; i <= k; ++i)
+        if (dp1[n - 1][i] && str_comp(s.substr(n - i, i), ans))
+            ans = s.substr(n - i, i);
+
+    for (int i = 0; i <= k; ++i)
+        if (dp2[n - 1][i] && str_comp(s_rev.substr(n - i, i), ans))
+            ans = s_rev.substr(n - i, i);
+
+    cout << ans << '\n';
+}
+
+int32_t main()
+{
+    cin.tie(0);
+    std::ios_base::sync_with_stdio(false);
+
+    int32_t t{ 1 };
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
